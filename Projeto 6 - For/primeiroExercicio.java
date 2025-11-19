c20ede00998e63d71db90eda24f78d3100f019d2
miqueasluis0-cpu <miqueasluis0@gmail.com>
2025-11-19 00:20:02 -0300

    Add files via upload

---
+++ Projeto 6 - For/primeiroExercicio.java
@@ -0,0 +1,22 @@
+package prjlistaFor;
+
+import java.util.Scanner;
+
+public class primeiroExercicio {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int i;
+		
+		for(i=1;i<1000;i=i+2) {
+			
+			System.out.print("  "+i);
+			
+		}
+		
+
+
+	}
+
+}
